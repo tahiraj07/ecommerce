2107c37754a4c4af4365d8ba0a4ee3897d24a7aa
Islam Tahiraj [LI] <i.tahiraj@logicainformatica.it>
2023-05-16 21:36:24 +0200

    e-commerce

---
+++ CORE/Entities/Product.cs
@@ -0,0 +1,8 @@
+namespace CORE.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using CORE.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
